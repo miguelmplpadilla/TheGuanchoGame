132cab86536e94a4092969f8f388b8a096dae548
miguelmplpadilla <miguelpadillal@hotmail.es>
2023-04-04 18:27:15 +0200

    Arreglos niveles expo

--- The_Guancho_Game/Assets/Resources/Scripts/Personajes/Player/PlayerMovement.cs
+++ The_Guancho_Game/Assets/Resources/Scripts/Personajes/Player/PlayerMovement.cs
@@ -104,12 +104,12 @@ public class PlayerMovement : MonoBehaviour
             {
                 if (horizontalInput > 0f)
                 {
-                    transform.localScale = new Vector3(1, 1, 1);
+                    transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
                     textoPlayer.transform.localScale = new Vector3(1, 1, 1);
                 }
                 else if (horizontalInput < 0f)
                 {
-                    transform.localScale = new Vector3(-1, 1, 1);
+                    transform.localScale = new Vector3(-0.6f, 0.6f, 0.6f);
                     textoPlayer.transform.localScale = new Vector3(-1, 1, 1);
                 }
             }
