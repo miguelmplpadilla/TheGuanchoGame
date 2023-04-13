44c6393d65501f583d92564cfd7aada7ff7daa70
miguelmplpadilla <89489412+miguelmplpadilla@users.noreply.github.com>
2023-04-13 21:17:22 +0200

    Player controller que funciona con rampas

---
+++ The_Guancho_Game/Assets/Resources/Scripts/Personajes/Player/Movimiento/PlayerController.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private float groundCheckRadius;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float slopeCheckDistance;
+    [SerializeField]
+    private float maxSlopeAngle;
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    [SerializeField]
+    private PhysicsMaterial2D noFriction;
+    [SerializeField]
+    private PhysicsMaterial2D fullFriction;
+
+    private float xInput;
+    private float slopeDownAngle;
+    private float slopeSideAngle;
+    private float lastSlopeAngle;
+
+    private int facingDirection = 1;
+
+    private bool isGrounded;
+    private bool isOnSlope;
+    private bool isJumping;
+    private bool canWalkOnSlope;
+    private bool canJump;
+
+    private Vector2 newVelocity;
+    private Vector2 newForce;
+    private Vector2 capsuleColliderSize;
+
+    private Vector2 slopeNormalPerp;
+
+    private Rigidbody2D rb;
+    private CapsuleCollider2D cc;
+
+    public GameObject posicionLanzarRayCast;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cc = GetComponent<CapsuleCollider2D>();
+
+        capsuleColliderSize = cc.size;
+    }
+
+    private void Update()
+    {
+        CheckInput();     
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+        SlopeCheck();
+        ApplyMovement();
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        Flip();
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+    }
+    private void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if(rb.velocity.y <= 0.0f)
+        {
+            isJumping = false;
+        }
+
+        if(isGrounded && !isJumping && slopeDownAngle <= maxSlopeAngle)
+        {
+            canJump = true;
+        }
+
+    }
+
+    private void SlopeCheck()
+    {
+        Vector2 checkPos = posicionLanzarRayCast.transform.position - (Vector3)(new Vector2(0.0f, capsuleColliderSize.y / 2));
+
+        //SlopeCheckHorizontal(checkPos);
+        SlopeCheckVertical(checkPos);
+    }
+
+    private void SlopeCheckHorizontal(Vector2 checkPos)
+    {
+        RaycastHit2D slopeHitFront = Physics2D.Raycast(checkPos, transform.right, slopeCheckDistance, whatIsGround);
+        RaycastHit2D slopeHitBack = Physics2D.Raycast(checkPos, -transform.right, slopeCheckDistance, whatIsGround);
+
+        if (slopeHitFront)
+        {
+            Debug.Log("SlopeHitFront");
+            isOnSlope = true;
+
+            slopeSideAngle = Vector2.Angle(slopeHitFront.normal, Vector2.up);
+
+        }
+        else if (slopeHitBack)
+        {
+            Debug.Log("SlopeHitBack");
+            isOnSlope = true;
+
+            slopeSideAngle = Vector2.Angle(slopeHitBack.normal, Vector2.up);
+        }
+        else
+        {
+            Debug.Log("SlopeNoHit");
+            slopeSideAngle = 0.0f;
+            isOnSlope = false;
+        }
+
+    }
+
+    private void SlopeCheckVertical(Vector2 checkPos)
+    {      
+        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, whatIsGround);
+        
+        if (hit)
+        {
+            Debug.Log("Hit: "+hit.collider.name);
+            
+            slopeNormalPerp = Vector2.Perpendicular(hit.normal).normalized;            
+
+            slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if(slopeDownAngle != lastSlopeAngle)
+            {
+                isOnSlope = true;
+            }                       
+
+            lastSlopeAngle = slopeDownAngle;
+           
+            Debug.DrawRay(hit.point, slopeNormalPerp, Color.blue);
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
+
+        }
+
+        if (slopeDownAngle > maxSlopeAngle || slopeSideAngle > maxSlopeAngle)
+        {
+            canWalkOnSlope = false;
+        }
+        else
+        {
+            canWalkOnSlope = true;
+        }
+
+        canWalkOnSlope = true;
+
+        Debug.Log("XInput: "+xInput);
+        
+        if (isOnSlope && canWalkOnSlope && xInput == 0.0f)
+        {
+            rb.sharedMaterial = fullFriction;
+        }
+        else
+        {
+            rb.sharedMaterial = noFriction;
+        }
+    }
+
+    private void Jump()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            isJumping = true;
+            newVelocity.Set(0.0f, 0.0f);
+            rb.velocity = newVelocity;
+            newForce.Set(0.0f, jumpForce);
+            rb.AddForce(newForce, ForceMode2D.Impulse);
+        }
+    }   
+
+    private void ApplyMovement()
+    {
+        Debug.Log("IsOnSlope: "+isOnSlope+" IsGrounded: "+isGrounded+" IsJumping: "+isJumping+" CanWalkOnSlope: "+canWalkOnSlope);
+        if (isGrounded && !isOnSlope && !isJumping) //if not on slope
+        {
+            Debug.Log("No rampa");
+            newVelocity.Set(movementSpeed * xInput, 0.0f);
+            rb.velocity = newVelocity;
+        }
+        else if (isGrounded && isOnSlope && canWalkOnSlope && !isJumping) //If on slope
+        {
+            Debug.Log("Rampa");
+            newVelocity.Set(movementSpeed * slopeNormalPerp.x * -xInput, movementSpeed * slopeNormalPerp.y * -xInput);
+            rb.velocity = newVelocity;
+        }
+        else if (!isGrounded) //If in air
+        {
+            Debug.Log("Aire");
+            newVelocity.Set(movementSpeed * xInput, rb.velocity.y);
+            rb.velocity = newVelocity;
+        }
+
+    }
+
+    private void Flip()
+    {
+        if (xInput > 0f)
+        {
+            transform.localScale = new Vector3(1,1,1);
+        }
+        else if (xInput < 0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+}
+
