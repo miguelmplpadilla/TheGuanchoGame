205c91033d801ce1079dfaeac4b99c3c02ee9e7b
miguelmplpadilla <89489412+miguelmplpadilla@users.noreply.github.com>
2023-05-25 12:09:01 +0200

    Cinematica inicial

---
+++ The_Guancho_Game/Assets/Resources/Scripts/Cinematicas/Inicio/CinematicaInicioController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicaInicioController : MonoBehaviour
+{
+    [SerializeField] private GameObject timeline1;
+    [SerializeField] private GameObject timeline2;
+    [SerializeField] private GameObject timeline3;
+    
+    [SerializeField] private GameObject[] objetosDesactivarTimeline1;
+    [SerializeField] private GameObject[] objetosDesactivarTimeline2;
+
+    public void activarTimeline2()
+    {
+        timeline1.SetActive(false);
+        
+        for (int i = 0; i < objetosDesactivarTimeline1.Length; i++)
+        {
+            objetosDesactivarTimeline1[i].SetActive(false);
+        }
+        
+        timeline2.SetActive(true);
+    }
+    
+    public void activarTimeline3()
+    {
+        timeline2.SetActive(false);
+        
+        for (int i = 0; i < objetosDesactivarTimeline2.Length; i++)
+        {
+            objetosDesactivarTimeline2[i].SetActive(false);
+        }
+        
+        timeline3.SetActive(true);
+    }
+}
