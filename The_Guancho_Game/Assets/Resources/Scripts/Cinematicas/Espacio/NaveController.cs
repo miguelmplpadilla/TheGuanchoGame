af477d200dd49c66eb1314a21e88228572051120
miguelmplpadilla <89489412+miguelmplpadilla@users.noreply.github.com>
2023-06-01 11:43:07 +0200

    Videos añadidos a las cinematicas

--- The_Guancho_Game/Assets/Resources/Scripts/Cinematicas/Espacio/NaveController.cs
+++ The_Guancho_Game/Assets/Resources/Scripts/Cinematicas/Espacio/NaveController.cs
@@ -1,12 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class NaveController : MonoBehaviour
 {
-    public void moverEscena(string nombreEscena)
+
+    [SerializeField] private VideoPlayer video;
+
+    [SerializeField] private string nombreEscena = "";
+
+    public void moverEscena()
     {
         SceneManager.LoadScene(nombreEscena);
+    }
+
+    public void iniciarCinematica()
+    {
+        video.Play();
+
+        Invoke("moverEscena", (float)video.clip.length);
     }
 }
